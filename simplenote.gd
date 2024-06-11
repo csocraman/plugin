bd99e7939cd9b80506f68002e3e62de2ac73d34c
csocraman <addol7150@gmail.com>
2024-06-11 12:26:08 -0300

    changes

---
+++ simplenote.gd
@@ -0,0 +1,27 @@
+@tool
+extends EditorPlugin
+
+var note = preload("res://note.tscn")
+var noteins
+
+func _enter_tree():
+	noteins = note.instantiate()
+	get_editor_interface().get_editor_main_screen().add_child(noteins)
+	_make_visible(false)
+	
+func _get_plugin_icon():
+	return preload("res://addons/simplenote/Frame 1icon.svg")
+
+func _get_plugin_name():
+	return "simple note"
+
+func _has_main_screen():
+	return true
+
+func _make_visible(visible):
+	if is_instance_valid(noteins):
+		noteins.visible = visible
+
+func _exit_tree():
+	if is_instance_valid(noteins):
+		noteins.queue_free()
